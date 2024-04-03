4bfd1ba3a8e26228a94673bff9dab54236b75cb8
Razalp <razalp0012300@gmail.com>
2024-04-03 17:40:31 +0530

    CLEAN ARCH FOLDER SET

---
+++ Backend/index.ts
@@ -0,0 +1,6 @@
+// index.ts
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("World"));
--- FrontEnd/vite-project/src/Pages/UserSide/NavBar/NavBar.tsx
+++ FrontEnd/vite-project/src/Pages/UserSide/NavBar/NavBar.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
 
 const NavBar = () => {
   return (
     <Navbar className="bg-black">
       <NavbarBrand>
-        {/* <Logo /> */}
-        <p className="font-bold text-inherit">COS</p>
+        
+        <p className="font-bold text-inherit text-white">COS</p>
+        <p className="font-bold text-inherit text-white">&nbsp;&nbsp;▷◁</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         {/* <NavbarItem>
